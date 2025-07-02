7ee94d94a4702c82a65b032d7c41ee77989a6ca9
Esteban Rios <esteban_690@hotmail.com>
2025-07-02 19:34:11 -0300

    Creacion de la pagina de detalles de eventos
    Se crea una pagina para ver los detalles del evento

--- proyfrontendgrupo08/src/app/app.routes.ts
+++ proyfrontendgrupo08/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PagesComponent } from './page/page.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { EventoComponent } from './evento/evento.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'inicio', pathMatch: 'full' },
     {path: 'inicio', component: PagesComponent},
     {path: 'eventos', component: EventosComponent},
+    {path: 'evento/:id', component: EventoComponent},
 ];
